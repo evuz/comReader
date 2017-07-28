40facd10e0be4c24d0ee4b9957af1ea97e0ae9c4
evuz <jesus_jgb_45@hotmail.com>
2017-07-28 13:33:28 +0200

    Welcome component

---
+++ src/renderer/app/components/Welcome/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.scss';
+
+const WelcomeComponent = () => (
+  <div className="welcome_component">
+    <p>Welcome to Visual Comic Reader</p>
+  </div>
+);
+
+export default WelcomeComponent;
