4d63b66b1c53dae9375c26a0f3521d02aec0da0f
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-05-05 20:12:13 +0200

    SASS to styled-component: Loading Component

--- src/renderer/app/components/Loading/index.js
+++ src/renderer/app/components/Loading/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Spinner from 'react-spinkit';
 
-import './index.scss';
+import { Root } from './components';
 
-const Loading = ({ state }) => (
-  state ?
-    <div className="loading_component">
-      {<Spinner name="double-bounce" style={styles.spinner} />}
-    </div> :
-    null
-);
-
+const Loading = (props) => {
+  const { state } = props;
+  return state ? (
+    <Root>
+      <Spinner name="double-bounce" style={styles.spinner} />
+    </Root>
+  ) : null;
+};
 const styles = {
   spinner: {
     width: '80px',
