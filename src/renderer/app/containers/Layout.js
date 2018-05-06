fa582d0f1587c8e4d97de53dc125adeefa73dc09
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-05-07 00:12:22 +0200

    SASS to styled-component: Layout

--- src/renderer/app/containers/Layout.js
+++ src/renderer/app/containers/Layout.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import LayoutComponent from '../components/Layout';
 import ControlNavContainer from './ControlNav';
 import LateralPanelContainer from './LateralPanel';
 
-const LayoutContainer = ({ children, fullScreen }) => (
+const LayoutContainer = ({ children }) => (
   <LayoutComponent
     controlNav={<ControlNavContainer />}
     lateralPanel={<LateralPanelContainer />}
-    fullScreen={fullScreen}
   >
     {children}
   </LayoutComponent>
 );
 
-const mapStateToProps = state => ({
-  fullScreen: state.windowState.fullScreen,
-});
-
-export default connect(mapStateToProps)(LayoutContainer);
+export default LayoutContainer;
